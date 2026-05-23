579d0b2efcef5d917546085393a75bb98efdd472
AtharvM2411 <atharvmargur@gmail.com>
2026-05-23 21:53:55 +0530

    Refactor header files: add include guards to cbc.h, config.h, fileio.h, processor.h, and xtea.h; reorganize packing/unpacking test case

--- include/cbc.h
+++ include/cbc.h
@@ -1,3 +1,6 @@
+#ifndef CBC_H
+#define CBC_H
 #include <stdint.h>
 #include "config.h"
 void xor_block(uint32_t block_plain[2],const uint32_t block_prev[2]);
+#endif //CBC_H
--- include/processor.h
+++ include/processor.h
@@ -1,3 +1,5 @@
+#ifndef PROCESSOR_H
+#define PROCESSOR_H
 #include <string.h>
 #include"fileio.h"
 struct file_info_st get_file_info();
@@ -5,3 +7,4 @@ void encrypt(struct file_info_st file_info);
 void decrypt(struct file_info_st file_info);
 void bytes_to_words(uint8_t file_chunk[],uint32_t file_word[],size_t bytes_per_word);
 void words_to_bytes(const uint32_t file_word[],uint8_t file_chunk[],size_t bytes_per_word);
+#endif //PROCESSOR_H
